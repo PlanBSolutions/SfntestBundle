6a1837d06861ef2471755fc338bc20977f7c195a
PlanBSolutions <80087953+PlanBSolutions@users.noreply.github.com>
2021-03-05 02:57:51 -0300

    Create StockManagerService.php

---
+++ src/Services/StockManagerService.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Planb\SfntestBundle\Services;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use App\Planb\SfntestBundle\Entity\StockManager;
+use App\Planb\SfntestBundle\Message\LowStockNotitication;
+
+class StockManagerService
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+    
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+    
+    /**
+     * Processing stock insert or update
+     * 
+     * @param StockManager $stockItem
+     * @return string
+     */
+    public function processStock($stockItem)
+    {
+        $objMan     = $this->container->get('doctrine')->getManager();
+        $dublicate  = $objMan->getRepository(StockManager::class)->findDublicate($stockItem);
+        
+        if ($dublicate && $dublicate->getId()) {
+            /* Update if dublicate */
+            $dublicate->setStock($stockItem->getStock());
+            $objMan->persist($dublicate);
+            
+            $successMessage = 'Updated Stock';
+        }
+        else {
+            $objMan->persist($stockItem);
+            
+            $successMessage = 'Added Stock';
+        }
+        
+        if ($stockItem->getStock() <= 0) {
+            /* Low stock warning message */
+            $this->container->get('messenger.default_bus')->dispatch(
+                new LowStockNotitication('SKU '.$stockItem->getSKU().' run out of stock in the '.$stockItem->getBranch().' branch.')
+            );
+        }
+        
+        $objMan->flush();
+        
+        return $successMessage;
+    }
+}
