3b00d933bf135715761436e149dedf6bce8ac643
PlanBSolutions <80087953+PlanBSolutions@users.noreply.github.com>
2021-03-05 02:56:51 -0300

    Create LowStockNotiticationHandler.php

---
+++ src/MessageHandler/LowStockNotiticationHandler.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Planb\SfntestBundle\MessageHandler;
+
+use App\Planb\SfntestBundle\Message\LowStockNotitication;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class LowStockNotiticationHandler implements MessageHandlerInterface
+{
+    /**
+     * @var MailerInterface 
+     */
+    private $mailer;
+    
+    /**
+     * @param MailerInterface $mailer - for email transport
+     */
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+    
+    /**
+     * @param LowStockNotitication $message
+     */
+    public function __invoke(LowStockNotitication $message)
+    {
+        $email = new Email();
+        
+        $email->subject('Low Stock Warning')->text($message->getContent());
+        
+        $this->mailer->send($email);
+    }
+}
+
