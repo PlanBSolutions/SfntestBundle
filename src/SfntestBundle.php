93842e6f651131d53105d361796411977250a336
PlanBSolutions <80087953+PlanBSolutions@users.noreply.github.com>
2021-03-05 03:47:38 -0300

    Add files via upload

---
+++ src/SfntestBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Planb\SfntestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PlanbSfntestBundle extends Bundle
+{
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
+}
